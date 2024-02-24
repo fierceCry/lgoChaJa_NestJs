97a75a6f2a5852946ff399d2967fd63082514d31
kimmangyu <vlsual0917@gmail.com>
2024-02-24 17:48:29 +0900

    Swagger ApiProperty users 완성

--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   Column,
   CreateDateColumn,
@@ -11,15 +12,31 @@ Index("email", ["email"], { unique: true })
 @Entity({ schema: "lgoChaja", name: "users" })
 export class Users {
 
+  @ApiProperty({
+    example: 1,
+    description : 'id'
+  })
   @PrimaryGeneratedColumn({ type: "int", name: "id" })
   id: number;
 
+  @ApiProperty({
+    example: 'vlsual0917@gmail.com',
+    description : '이메일'
+  })
   @Column("varchar", { name: "email", unique: true, length: 30 })
   email: string;
 
+  @ApiProperty({
+    example: 'kimmangyu',
+    description : '닉네임'
+  })
   @Column("varchar", { name: "nickname", length: 30 })
   nickname: string;
 
+  @ApiProperty({
+    example: 'password',
+    description : '비밀번호'
+  })
   @Column("varchar", { name: "password", length: 100, select: false })
   password: string;
 
