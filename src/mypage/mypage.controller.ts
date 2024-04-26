672e2f8b5b79c6d546227b33b7c3911b5be0a4c7
kimmangyu <vlsual0917@gmail.com>
2024-04-26 19:44:51 +0900

    패스워드 변경완료

--- src/mypage/mypage.controller.ts
+++ src/mypage/mypage.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
 import { MypageService } from './mypage.service';
 import { User } from 'src/common/decorators/user.decorator';
 import { Users } from 'src/entities/Users';
@@ -12,5 +12,17 @@ export class MypageController {
     @User() user: Users,
   ) {
     return await this.mypageService.findFollowedUsers(user.id);
+  }
+
+  @Patch('/setting/password')
+  async passwordChage(
+    @User() user: Users,
+    @Body('password') password: string,
+    @Body('newPassword') newPassword: string
+  ){
+    console.log(user.id)
+    console.log(password)
+    console.log(newPassword)
+    return await this.mypageService.password(user.id, password, newPassword)
   }
 }
--- src/mypage/mypage.module.ts
+++ src/mypage/mypage.module.ts
@@ -3,10 +3,11 @@ import { MypageService } from './mypage.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { MypageController } from './mypage.controller';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
   controllers: [MypageController],
-  providers: [MypageService]
+  providers: [MypageService, UsersService]
 })
 export class MypageModule {}
