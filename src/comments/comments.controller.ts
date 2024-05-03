5495473ddb424319e2830ae3189fe395091e9d30
kimmangyu <vlsual0917@gmail.com>
2024-05-03 17:00:21 +0900

    swagger 작성

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -2,13 +2,25 @@ import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { Users } from 'src/entities/Users';
 import { User } from 'src/common/decorators/user.decorator';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('COMMENTS')
 @Controller('comments')
 export class CommentsController {
   constructor(
     private readonly commentsService : CommentsService
   ){}
 
+  @ApiOperation({
+    summary : "게시글 댓글 생성"
+  })
+  @ApiResponse({
+    status: 200,
+    description : 
+    `
+    댓글 완료
+    `
+  })
   @Post('/post/:postId')
   async commentsCreate(
     @User() user: Users,
@@ -23,6 +35,16 @@ export class CommentsController {
     return {data: result};
   }
   
+  @ApiOperation({
+    summary : "댓글 수정"
+  })
+  @ApiResponse({
+    status: 200,
+    description : 
+    `
+    댓글 수정 완료
+    `
+  })
   @Patch('post/:postId/:commentsId')
   async commentsPatch(
     @User() user:Users,
@@ -39,6 +61,16 @@ export class CommentsController {
     return {data: result}
   }
 
+  @ApiOperation({
+    summary : "댓글 삭제"
+  })
+  @ApiResponse({
+    status: 200,
+    description : 
+    `
+    댓글 삭제 완료
+    `
+  })
   @Delete('post/:postId/comments/:commentsId')
   async commentsDelete(
     @User() user:Users,
--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,17 +1,54 @@
 import { Controller, Get, Param} from '@nestjs/common';
 import { MailService } from './mail.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('MAIL')
 @Controller('api/mail')
 export class MailController {
   constructor(
     private readonly mailService: MailService,
   ) {}
 
+  @ApiOperation({
+    summary : "유저 email 찾기"
+  })
+  @ApiResponse({
+    status: 200,
+    description : 
+    `
+    data: 인증코드를 전송했습니다.
+    `
+  })
+  @ApiResponse({
+    status: 404,
+    description : 
+    `
+    존재하지 않는 사용자입니다
+    `
+  })
   @Get('id/:email')
   async sendEmail(@Param('email') email: string) {
       return await this.mailService.sendEmail(email);
     }
 
+  
+    @ApiOperation({
+      summary : "유저 email 인증코드"
+    })
+    @ApiResponse({
+      status: 200,
+      description : 
+      `
+      data: TKD2KD
+      `
+    })
+    @ApiResponse({
+      status: 404,
+      description : 
+      `
+      data: 유효한 코드가 아닙니다.
+      `
+    })
   @Get('certify/:verificationCode/:email')
   async emailCertified(
     @Param('verificationCode') verificationCode: string,
@@ -20,6 +57,23 @@ export class MailController {
     return await this.mailService.verifyCode(verificationCode, email);
   }
 
+  @ApiOperation({
+    summary : "유저 password 찾기"
+  })
+  @ApiResponse({
+    status: 200,
+    description : 
+    `
+    data: 임시 비밀번호를 전송했습니다.
+    `
+  })
+  @ApiResponse({
+    status: 404,
+    description : 
+    `
+      이메일 전송 중 오류가 발생했습니다.
+    `
+  })
   @Get('password/:email')
   async sendPasswordResetEmail(@Param('email') email: string){
     return await this.mailService.sendPasswordResetEmail(email);
