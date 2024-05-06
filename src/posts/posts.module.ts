6d66fd523130f5363ca35baa322ed0b929bf6651
kimmangyu <vlsual0917@gmail.com>
2024-05-06 16:01:46 +0900

    코드 정리완성

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,9 +4,11 @@ import { Post } from 'src/entities/Post';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsController } from './posts.controller';
 import { PostImages } from 'src/entities/PostImages';
+import { Users } from 'src/entities/Users';
+import { PostsComments } from 'src/entities/PostsComments';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, PostImages])],
+  imports: [TypeOrmModule.forFeature([Post, PostImages, Users, PostsComments])],
   controllers: [PostsController],
   providers: [PostsService]
 })
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from 'src/entities/Post';
 import { PostImages } from 'src/entities/PostImages';
+import { PostsComments } from 'src/entities/PostsComments';
+import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
 @Injectable()
 export class PostsService {
@@ -10,6 +12,10 @@ export class PostsService {
     private postRepository: Repository<Post>,
     @InjectRepository(Post)
     private postImageRepository: Repository<PostImages>,
+    @InjectRepository(Users)
+    private userRepository: Repository<Users>,
+    @InjectRepository(PostsComments)
+    private postCommentsRepository: Repository<PostsComments>,
   ) {}
 
   // 게시글 생성
@@ -48,23 +54,37 @@ export class PostsService {
         .createQueryBuilder('post')
         .innerJoinAndSelect('post.user', 'u')
         .innerJoinAndSelect('post.postsCategorys', 'pc')
+        .innerJoinAndSelect('post.postImages', 'pi')
         .select([
-          'post.id', 
+          'post.id',
           'post.userId', 
           'post.postTitle', 
           'post.postContent', 
-          'post.tags'
+          'post.tags',
+          'pi.imageUrl',
+          'u.nickname',
+          'pc.categoryName'
         ])
-        .addSelect('u.nickname')
-        .addSelect('pc.category_name')
         .getMany();
-      return posts;
+
+        const modifiedPosts = posts.map(post => {
+          return {
+            postId: post.id,
+            userId: post.userId,
+            title: post.postTitle,
+            content: post.postContent,
+            tags: post.tags,
+            postImages: post.postImages.map(image => image.imageUrl),
+            userNickname: post.user.nickname,
+            categoryName: post.postsCategorys.categoryName
+          };
+        });
+      return modifiedPosts;
     } catch {
       throw new NotFoundException('조회할 수 없습니다.');
     }
   }
-  
-  
+    
   // 게시글 수정
   async modifyPost(
     userId: number,
@@ -75,6 +95,14 @@ export class PostsService {
     postsCategorys: number,
     tags: string,
   ) {
+
+    const result = await this.userRepository.findOne({
+      where: {id:userId},
+      select: ['id']
+    })
+    if(!result){
+      throw new ForbiddenException("수정할 권한이 없습니다.");
+    }
     await this.postRepository.query(
       `UPDATE post
     SET
@@ -107,7 +135,7 @@ export class PostsService {
       where: { id: postId, userId: userId }
     });
     if (!post) {
-      throw new Error('글이 존재하지 않거나 해당 사용자에 의해 생성된 글이 아닙니다.');
+      throw new ForbiddenException('글이 존재하지 않거나 해당 사용자에 의해 생성된 글이 아닙니다.');
     }
     await this.postImageRepository.query(
       `
@@ -116,7 +144,13 @@ export class PostsService {
       `, 
       [postId]
     );
-  
+    await this.postCommentsRepository
+    .createQueryBuilder()
+    .delete()
+    .from(PostsComments)
+    .where("post_id = :postId", { postId: postId })
+    .andWhere("user_id = :userId", { userId: userId })
+    .execute();
     await this.postRepository.delete({ id: postId });
     return "게시글 삭제 완료";
   }
